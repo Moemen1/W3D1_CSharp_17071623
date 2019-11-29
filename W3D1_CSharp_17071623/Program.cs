8caff2ddd7c798e6998db7bf52eafc3ede9a036a
Moemen1 <moemenbadawi@hotmail.com>
2019-11-29 16:49:12 +0100

    Add project files.

---
+++ W3D1_CSharp_17071623/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace W3D1_CSharp_17071623
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
